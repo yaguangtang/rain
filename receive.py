d0d310db8913f05c21a8e7b54f430b3cc736f501
ljj-justin <iamljj@gmail.com>
2012-06-13 12:05:08 +0800

    add send message method.

--- receive.py
+++ receive.py
@@ -1,22 +1,25 @@
+#!/usr/bin/python
+
 import pika
 import sys
 
 
-cred=pika.credentials.PlainCredentials(username='guest',password='beyond630')
+cred=pika.credentials.PlainCredentials(username='guest',password='7d0fe7e2f9c405734dd41d84cb91e877')
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(
-        host='localhost', credentials=cred))
+        host='172.16.55.131', credentials=cred))
 channel = connection.channel()
-channel.queue_bind(exchange='nova',
-                       queue='notifications.error',
-                       routing_key='*')
+channel.queue_delete(queue='notifications.error')
+#channel.queue_bind(exchange='nova',
+#                       queue='notifications.error',
+#                       routing_key='notifications.error')
 
 def callback(ch, method, properties, body):
     print " [x] %r:%r" % (method.routing_key, body,)
 
-channel.basic_consume(callback,
-                      queue='notifications.error',
-                      no_ack=True)
+#channel.basic_consume(callback,
+#                      queue='',
+#                      no_ack=True)
 
-channel.start_consuming()
+#channel.start_consuming()
 
